fe258342ecaa0d6c42f1373e72d1ce2527f2292d
leetuandat <tuandat24034@gmail.com>
2024-08-23 14:15:59 +0700

    config method, add chart

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ import { paging } from "./pages.js";
 import { sort } from "./sort.js";
 import { searchUser } from "./search.js";
 import { deleteUser } from "./delete.js";
+import { editUser } from "./edit.js";
+import { drawChart } from "./chart.js";
 let  API_User = `http://localhost:3000/users?q=${params.q}&_sort=${params.sort}&_order=asc&_page=${params.page}&_limit=${params.limit}`;
 
 //tinh nang tim kiem
@@ -12,12 +14,16 @@ searchUser(API_User);
 sort(API_User);
 //tinh nang phan trang
 paging(API_User)
+//ve bieu do
+drawChart()
 //xoá
+
 //start
 
 
-const User =async  ()=>{
+const User = async  ()=>{
   await DrawUser(API_User)
   deleteUser();
+  editUser();
 }
 User();
